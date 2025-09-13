a3ce2840903406910cc50e66d2074abf1848104b
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-09-12 21:40:05 -0300

    feat: añadir relaciones: (ManyToOne), (ManyToMany)

--- sistema-gestion-empleados/src/main/java/com/empresa/gestionempleados/entidades/Empleado.java
+++ sistema-gestion-empleados/src/main/java/com/empresa/gestionempleados/entidades/Empleado.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "empleados")
@@ -32,4 +34,16 @@ public class Empleado {
 
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal salario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departamento_id")
+    private Departamento departamento;
+
+    @ManyToMany
+    @JoinTable(
+            name = "empleado_proyecto",
+            joinColumns = @JoinColumn(name = "empleado_id"),
+            inverseJoinColumns = @JoinColumn(name = "proyecto_id")
+    )
+    private Set<Proyecto> proyectos = new HashSet<>();
 }
