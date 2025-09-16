324d1fbf42d090baa0e609f5dd7e920a36c04169
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-09-16 19:41:17 -0300

    feat: añadir manejador global de excepciones

--- sistema-gestion-empleados/src/main/java/com/empresa/gestionempleados/exceptions/GlobalExceptionHandler.java
+++ sistema-gestion-empleados/src/main/java/com/empresa/gestionempleados/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.empresa.gestionempleados.controladores;
+package com.empresa.gestionempleados.exceptions;
 
-import com.empresa.gestionempleados.exceptions.DepartamentoNoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +10,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DepartamentoNoEncontradoException.class)
     public ResponseEntity<String> manejarDepartamentoNoEncontrado(DepartamentoNoEncontradoException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EmpleadoNoEncontradoException.class)
+    public ResponseEntity<String> manejarEmpleadoNoEncontrado(EmpleadoNoEncontradoException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ProyectoNoEncontradoException.class)
+    public ResponseEntity<String> manejarProyectoNoEncontrado(ProyectoNoEncontradoException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
